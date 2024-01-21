8b326fb27433e476570fa9c2d7fca0a0c16cf045
Manson <Manson@LAPTOP-ASUS8888>
2024-01-21 18:25:47 -0500

    Tony :anded new class and git ignore

---
+++ MortgageCalculatorGitPractice/src/main/java/com/application/Addition.java
@@ -0,0 +1,10 @@
+package com.application;
+
+public class Addition {
+
+	public void add(int x, int y) {
+		int result = x + y;
+		System.out.println("x: " + x + " + y: " + y + "= result:" + result);
+	}
+	
+}
