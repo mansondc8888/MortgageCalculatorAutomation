e02d032889b7a6218a54327111081dd426c25add
Manson <Manson@LAPTOP-ASUS8888>
2024-01-21 20:49:21 -0500

    Frank: update addition class and main app

--- MortgageCalculatorGitPractice/src/main/java/com/application/Addition.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/Addition.java
@@ -2,9 +2,10 @@ package com.application;
 
 public class Addition {
 
-	public void add(int x, int y) {
+	public int added(int x, int y) {
 		int result = x + y;
-		System.out.println("x: " + x + " + y: " + y + "= result:" + result);
+		System.out.println("x is -" + x + ",y is -" + y);
+		return result;
 	}
 	
 }
--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -8,6 +8,10 @@ public class MortgageCalculatorApp {
 	public static void main(String[] args) {
 		
 		System.out.println("Mortgage Calculator Application is starting...");
+	
+		Addition add = new Addition();
+		int result = add.added(5, 8);
+		System.out.println("x + y = result ---->" + result);
 		
 	}
 	
